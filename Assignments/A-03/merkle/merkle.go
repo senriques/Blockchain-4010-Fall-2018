c95f0ccf4fa8709de5afd39023f5c98338d55b5d
Philip Schlump <pschlump@gmail.com>
2018-08-24 07:15:05 -0600

    Assignemnt 3

---
+++ Assignments/A-03/merkle/merkle.go
@@ -0,0 +1,13 @@
+package merkle
+
+/*
+import (
+	"fmt"
+
+	"github.com/Univ-Wyo-Education/Blockchain-4010-Fall-2018/Assignments/A-03/hash"
+)
+*/
+
+func MerkleHash(data [][]byte) []byte {
+	return InstructorMerkleHash(data)
+}
